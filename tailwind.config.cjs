66ceae63d7e62e4bb21ab82fcee0801769c0bd34
tigawanna <denniskinuthiaw@gmail.com>
2022-11-20 11:22:01 +0300

    vercel and tailwinf teme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +8,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-        require('tailwind-scrollbar'),
-  ],
+  plugins: [require("tailwind-scrollbar")],
 };
