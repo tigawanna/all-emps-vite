4a6cdea7968d5f32d64f2c03a00cac31fa478efd
tigawanna <denniskinuthiaw@gmail.com>
2022-11-23 08:45:29 +0300

    fixed useInfiniteQuery custome hook types

---
+++ src/components/posts/PostCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface PostCardProps {
+
+}
+
+export const PostCard: React.FC<PostCardProps> = ({}) => {
+return (
+ <div className='w-full '>
+
+ </div>
+);
+}
--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { fetchPosts } from '../../pb/useCollection';
+import { Record } from 'pocketbase';
+import { ListResult } from 'pocketbase';
+import { usePaginatedCollection } from './../../pb/useCollection';
+
 interface PostsProps {
 
 }
 
 export const Posts: React.FC<PostsProps> = ({}) => {
+  const pagquery = usePaginatedCollection({
+    key:['posts'],
+    rqOptions:{
+        enabled:true,
+        select: (data) => {
+        return data
+      } 
+       }
+  }
+  )
+
+
+  const query = useInfiniteQuery<ListResult<Record>, unknown, ListResult<Record>, string[]> 
+  (['posts'],fetchPosts,
+  
+  )
+  console.log("query =====>  ",query.data)
+
 return (
  <div>
    all posts
