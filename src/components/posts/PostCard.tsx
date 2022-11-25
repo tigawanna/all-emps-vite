4f2a05f8f1454a6a99472e720c2cd9fee8ceddae
tigawanna <denniskinuthiaw@gmail.com>
2022-11-25 18:46:50 +0300

    added post liking

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -19,32 +19,34 @@ interface PostCardProps {
     user: Record | Admin | null | undefined
 }
 
+
 export const PostsCard: React.FC<PostCardProps> = ({ item,user }) => {
     // console.log("url === ", makeUrl(item))
 
-
-
-
-    return (
-        <div className='w-[90%] md:w-[50%] p-2 flex flex-col  border-[2px] rounded-sm gap-1'>
-            <div className='w-full flex'>
-                <div className='flex text-xl font-bold '>
+return (
+        <div className='w-[90%] md:w-[50%] p-2 flex flex-col  border-black border-2  dark:border-white
+        rounded-lg gap-3'>
+            <div className='w-full flex flex-col gap-[1px]'>
+                <div className='flex text-sm font-bold'>
                     {item.expand?.emp?.name}
                 </div> 
-                <div className='flex text-lg '>
+                <div className='flex text-sm font-bold text-blue-900 dark:text-blue-300'>
                     @{item.expand?.emp?.username}
                 </div> 
                
             </div>      
-            <div className='w-full  flex text-lg font-bold '>
+            <div className='w-full  flex  text-sm '>
                 {item.body}
             </div>
 
             {item.media ? <img src={makeUrl(item)}
                 className='w-full h-fit bg-red-600' /> : null}
 
-            <div className='w-full  flex'>
-                {item.emp}
+            <div className='w-full  flex font-serif text-sm font-normal'>
+               emp id:  {item.emp}
+            </div>
+            <div className='w-full  flex font-serif text-sm font-normal'>
+                post id :  {item.id}
             </div>
             <div className='w-full  flex'>
              <PostReactionsCard user={user} item={item}/>
@@ -67,78 +69,121 @@ interface PostReactionsCardProps {
 user: Record | Admin | null | undefined
 item: PostType
 }
+interface ReactionResponse {
+    id: string
+    collectionId: string
+    collectionName: string
+    created: string
+    updated: string
+    post: string
+    emp: string
+    like: "yes" | "no";
+    comment: string
+}
+interface ReactionRequest {
+reaction?:string    
+post:string,
+emp:string;
+like:"yes"|"no";
+comment:string
+}
 
 export const PostReactionsCard: React.FC<PostReactionsCardProps> = ({user,item}) => {
 // console.log("post ids === ",user?.id,item.id)
-    const fetchReaction = async () => {
+       
+const fetchOneReaction = async () => {
+    try{
         return await client.collection('reactions').getFirstListItem(
-            `emp="${user?.id}" && post="${item?.id}" `)
-            
-            }
+        `emp="${user?.id}" && post="${item?.id}" `)
+    }
+     catch(err){
+     console.log("error fetching one  ==> ",err)
+     return {} as any
+    }
+    }
 
-    const query = useQuery(['emp-reaction'], fetchReaction,{
-        enabled:(user?.id && item?.id)?true:false
-    })
+    const query_key = ['emp-reaction', user?.id, item?.id] 
+
+    const query = useQuery(query_key, fetchOneReaction,{})
     const queryClient = useQueryClient();
-
-    const reactMutation = useMutation(async () => {
+    
+    const updateReactionMutation = useMutation(async (vars:ReactionRequest) => {
+        // console.log("update vars =====> ", vars) 
         try {
-            // const result = await client.collection('emps').authWithPassword(
-            //     vars.payload.get('email') as string,
-            //     vars.payload.get('password') as string
-            // )
-            const result = client.collection('reactions').update(
-                query?.data?.id as string,{
-                    post: query?.data?.post as string,
-                    emp: query?.data?.emp as string,
-                    like: !query?.data?.like,
-                    comment: ""
-                })
-            // queryClient.setQueryData(['user'], () => result.record);
-          
-        }
+             await client.collection('reactions').update(vars?.reaction as string,vars)}
         catch (err: any) {
             console.log("error in login mutation catch block", err.message)
             // setError({ name: "main", message: err?.messge })
             throw err
-        }
-
-
- 
-    },
+          } 
+          },
         {
             onSettled: () => {
-                queryClient.invalidateQueries(["user"]);
+                queryClient.invalidateQueries(query_key);
             },
             onError: (err: any) => {
            console.log("error updating ===> ",err)
             }
         }
     )
+    const newReactionMutation = useMutation(async (vars:ReactionRequest) => {
+        // console.log("create vars =====> ",vars) 
+        try { await client.collection('reactions').create(vars)}
+        catch (err: any) {
+            console.log("error in login mutation catch block", err.message)
+            // setError({ name: "main", message: err?.messge })
+            throw err
+        }
+    },
+    {
+    onSettled: () => {
+        queryClient.invalidateQueries(query_key);
+    },
+    onError: (err: any) => {
+        console.log("error updating ===> ", err)
+    }
+    }
+    )
 
    if(query.isLoading){
     return (
         <div className='w-full flex items-center justify-evenly'>
-            <TheIcon Icon={AiOutlineHeart}
-                size='1.5rem' color={""} iconAction={()=>{
-                    if(reaction){
-                        reactMutation.mutate()
-                    }
-                }} />
-            <TheIcon Icon={VscComment} size='1.5rem' />
+           ...
         </div>
     )
    }
 
    
-const reaction = query.data
+const reaction = query.data as ReactionResponse
+// console.log("raction ====== ",reaction)
+
+const reaction_vars:ReactionRequest ={
+post:reaction.post??item?.id,
+emp:reaction.emp??user?.id,
+like: reaction?.like ==="yes"?"no":"yes",
+comment:"",
+
+reaction: reaction.id,
+}
 
 return (
  <div className='w-full p-1'>
 <div className='w-full flex items-center justify-evenly'>
-<TheIcon Icon={reaction?.like ? AiFillHeart : AiOutlineHeart} 
-    size='1.5rem' color={reaction?.like ? 'red':""}/>
-<TheIcon Icon={VscComment} size='1.5rem' />
+    
+    <TheIcon 
+    Icon={reaction?.like==="yes" ? AiFillHeart :AiOutlineHeart} 
+    size='1.5rem'
+    color={reaction?.like ==="yes"?"red":""}
+    iconAction={()=>{
+        if (reaction?.like){
+         updateReactionMutation.mutate(reaction_vars)
+        }
+        else{
+         newReactionMutation.mutate(reaction_vars)
+        }
+    }}
+     />
+    <TheIcon Icon={VscComment} size='1.5rem' />
 </div>
  </div>
 );
--- src/pages/root/RootLayout.tsx
+++ src/pages/root/RootLayout.tsx
@@ -11,7 +11,7 @@ export const RootLayout: React.FC<RootLayoutProps> = ({}) => {
   // const load_data = useLoaderData()
   // console.log("loader data ====== > ",load_data)
   return (
-    <div className='h-full w-full overflow-y-scroll scroll-bar'>
+    <div className='h-full w-full overflow-y-scroll scroll-bar dark:bg-slate-900'>
       <div className='h-16 w-full  bg-slate-700 dark:bg-slate-800  
       bg-opacity-90 dark:bg-opacity-90 max-h-[50px] p-1
        sticky top-0 z-40'>
