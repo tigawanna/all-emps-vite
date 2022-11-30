1016eabf992097459972c76ad371aa554d5c527c
tigawanna <denniskinuthiaw@gmail.com>
2022-11-30 14:42:10 +0300

    fixed liking more than once issue

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -3,11 +3,10 @@ import { CustomPostType, RecordItem } from './../../pages/posts/Posts';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { VscComment } from 'react-icons/vsc'
 import { TheIcon } from '../../shared/TheIcon';
-import { PostType } from './types';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { client } from './../../pb/config';
 import { Record, Admin } from 'pocketbase';
-import { QueryClient } from '@tanstack/react-query';
+
 
 
 interface PostCardProps {
@@ -23,8 +22,12 @@ return (
         <div className='w-[90%] md:w-[50%]  p-2 flex flex-col  border-black border-2 
         dark:border-[1px]  dark:border-white
         rounded-lg gap-3'>
-            <div className='w-full flex flex-col gap-[1px]'>
-                <div className='flex text-sm font-bold'>
+            <div className='w-full flex justify-start itemscenter gap-[1px]'>
+            <div className='flex items-center justify-center '>
+                {item.creator_image ? <img src={makeUrl('emps',item.creator_id,item.creator_image)}
+                    className=' w-fit max-h-10 rounded-full aspect-square' /> : null}
+            </div>
+                <div className='flex items-center text-blue-700 justifycenter text-md font-bold px-2'>
                 {item.creator_name}
                 </div> 
                 {/* <div className='flex text-sm font-bold text-blue-900 dark:text-blue-300'>
@@ -36,7 +39,7 @@ return (
                 {item.post_body}
             </div>
         <div className='w-full  flex items-center justify-center '>
-            {item.creator_image ? <img src={makeUrl(item)}
+            {item.creator_image ? <img src={makeUrl('posts',item.post_id,item.post_media)}
                 className=' w-fit max-h-80 rounded-lg' /> : null}
            </div>
             <div className='w-full  flex font-serif text-sm font-normal'>
@@ -60,10 +63,10 @@ return (
 
 
 
-const makeUrl = (record: RecordItem|CustomPostType) => {
+const makeUrl = (coll_name:string,coll_id:string,media:string) => {
 
-    if (record?.creator_image) {
-        return `https://emps.tigawanna.tech/api/files/posts/${record.post_id}/${record?.post_media}`
+    if (media) {
+        return `https://emps.tigawanna.tech/api/files/${coll_name}/${coll_id}/${media}`
     }
     return
 
@@ -94,13 +97,20 @@ liked:"yes"|"no";
 
 export const PostReactionsCard: React.FC<PostReactionsCardProps> = ({user,item}) => {
 // console.log("post ids === ",user?.id,item.id)
+// console.log("item ===== ",item)
 const queryClient = useQueryClient()
 const [liked, setLiked] = React.useState(item.mylike === "yes")
-const updateReactionMutation = useMutation(async (vars:ReactionRequest) => {
-        // console.log("update vars =====> ", vars) 
-        try {
-             await client.collection('reactions').update(vars?.reaction as string,vars)}
-        catch (err: any) {
+
+    const updateReactionMutation = useMutation(async (vars: CustomPostType) => {
+    
+      const updatevars={
+        liked: item.mylike === "yes"?"no":"yes"
+        }
+      try {
+        await client.collection('reactions').update(vars?.reaction_id as string,updatevars)
+    // console.log("update reaction response === ",response)
+       }
+       catch (err: any) {
             console.log("error in login mutation catch block", err.message)
             // setError({ name: "main", message: err?.messge })
             throw err
@@ -116,9 +126,17 @@ const updateReactionMutation = useMutation(async (vars:ReactionRequest) => {
             }
         }
     )
-const newReactionMutation = useMutation(async (vars:ReactionRequest) => {
-        // console.log("create vars =====> ",vars) 
-        try { await client.collection('reactions').create(vars)}
+    const newReactionMutation = useMutation(async (vars: CustomPostType) => {
+        // console.log("create vars =====> ",vars)
+        const newReaction={
+            post:vars.post_id,
+            emp:vars.creator_id,
+            liked: "yes"
+        } 
+        try { 
+        await client.collection('reactions').create(newReaction)
+            // console.log("new reaction response === ", response)
+         }
         catch (err: any) {
             console.log("error in login mutation catch block", err.message)
             // setError({ name: "main", message: err?.messge })
@@ -136,18 +154,7 @@ const newReactionMutation = useMutation(async (vars:ReactionRequest) => {
     }
     )
 
-
-
-
-
 // console.log("total likes  ====== ",total_likes)
-
-const reaction_vars:ReactionRequest ={
-post:item.post_id,
-emp:item.creator_id??user?.id,
-liked: item?.mylike ==="yes"?"no":"yes",
-reaction:item.reaction_id,
-}
 
 return (
  <div className='w-full p-1'>
@@ -158,20 +165,17 @@ return (
                     size='1.5rem'
                     color={liked ? "red" : ""}
                     iconAction={() => {
-                        if (item?.mylike === 'yes') {
-                            updateReactionMutation.mutate(reaction_vars)
+                        if (item.reaction_id) {
+                            updateReactionMutation.mutate(item)
                             setLiked(prev => !prev)
                         }
                         else {
-                            newReactionMutation.mutate(reaction_vars)
+                            newReactionMutation.mutate(item)
                             setLiked(prev => !prev)
                         }
                     }}
                 />
-                {
-                    item.likes??0
-
-                }
+                { item.likes??0}
             </div>
 
     <TheIcon Icon={VscComment} size='1.5rem' />
